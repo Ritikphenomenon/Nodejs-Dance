d000e8875c88b58a2810de84a90de0767eb2afb3
Ritikphenomenon <97295964+Ritikphenomenon@users.noreply.github.com>
2022-10-17 19:03:06 +0530

    Delete tut84.js

--- tut84.js
+++
@@ -1,8 +0,0 @@
-show dbs
-use harrrykart
-show collections 
-db.items.find()
-//for updating the databases 
-db.items.updateOne({name:"moto 30s"},{$set:{price:2}}) 
-db.items.find()
-db.items.updateMany({name:"moto 30s"},{$set:{price:3,rating:1}})
